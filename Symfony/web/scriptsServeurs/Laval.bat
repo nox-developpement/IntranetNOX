f581007856df0acbd9d1d3db06b18c85781fbfb1
tristanbsn <tristan.besson@wanadoo.fr>
2016-03-25 09:40:57 +0100

    Sauvegarde BDD et scripts.

---
+++ Symfony/web/scriptsServeurs/Laval.bat
@@ -0,0 +1,3 @@
+net use /persistent:no
+net use y: /delete /y
+net use y: \\192.168.33.3\share\laval
---
+++ Symfony/web/scriptsServeurs/Nantes.bat
@@ -0,0 +1,3 @@
+net use /persistent:no
+net use y: /delete /y
+net use y: \\192.168.35.1\share\StHerblain
