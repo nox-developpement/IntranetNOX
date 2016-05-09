44fc0411dc3011b4c1157faf32008d1c49d376ac
tristanbsn <tristan.besson@wanadoo.fr>
2016-05-09 17:17:46 +0200

    Ajout sauvegarde données des feuilles de pointage + Début ajout de colonnes de projets.

--- Symfony/src/NoxIntranet/PointageBundle/Controller/PointageController.php
+++ Symfony/src/NoxIntranet/PointageBundle/Controller/PointageController.php
@@ -5,6 +5,7 @@ namespace NoxIntranet\PointageBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use NoxIntranet\PointageBundle\Entity\Tableau;
 
 class PointageController extends Controller {
 
@@ -25,6 +26,7 @@ class PointageController extends Controller {
                     'currentYear' => $this->getMonthAndYear($month, $year)['year']));
     }
 
+    // Retourne la date courante si aucune date n'est passé en paramètres.
     public function getMonthAndYear($month, $year) {
         if (empty($month)) {
             $month = date('m');
@@ -40,6 +42,7 @@ class PointageController extends Controller {
         return $date;
     }
 
+    // Retourne les jours et la date correspondante en fonction du moi et de l'année passés en paramètres.
     public function ajaxSetDateAction(Request $request) {
         if ($request->isXmlHttpRequest()) {
 
@@ -61,6 +64,57 @@ class PointageController extends Controller {
             $response = new Response(json_encode($monthDates));
             $response->headers->set('Content-Type', 'application/json');
 
+            return $response;
+        }
+    }
+
+    // Sauvegarde le contenu des cellules du tableau en fonction de l'utilisateur et de la date.
+    public function ajaxSaveDataAction(Request $request) {
+        if ($request->isXmlHttpRequest()) {
+            $month = $request->get('month');
+            $year = $request->get('year');
+            $data = $request->get('data');
+
+            $em = $this->getDoctrine()->getManager();
+            $user = $this->get('security.token_storage')->getToken()->getUser();
+
+            $tableData = $em->getRepository('NoxIntranetPointageBundle:Tableau')->findOneBy(array('user' => $user->getUsername(), 'month' => $month, 'year' => $year));
+
+            if (empty($tableData)) {
+                $tableData = new Tableau();
+
+                $tableData->setUser($user->getUsername());
+                $tableData->setMonth($month);
+                $tableData->setYear($year);
+            }
+
+            $tableData->setData($data);
+
+            $em->persist($tableData);
+            $em->flush();
+
+            $response = new Response($data);
+            return $response;
+        }
+    }
+
+    public function ajaxGetDataAction(Request $request) {
+        if ($request->isXmlHttpRequest()) {
+            $month = $request->get('month');
+            $year = $request->get('year');
+
+            $em = $this->getDoctrine()->getManager();
+            $user = $this->get('security.token_storage')->getToken()->getUser();
+
+            $tableData = $em->getRepository('NoxIntranetPointageBundle:Tableau')->findOneBy(array('user' => $user->getUsername(), 'month' => $month, 'year' => $year));
+
+            if (!empty($tableData)) {
+                $data = $tableData->getData();
+            } else {
+                $data = null;
+            }
+
+            $response = new Response($data);
             return $response;
         }
     }
