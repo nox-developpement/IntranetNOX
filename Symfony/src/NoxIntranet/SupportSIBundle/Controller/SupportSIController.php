fe4ceb364a4864ebd59e2f817d9c889eb992d8fb
tristanbsn <tristan.besson@wanadoo.fr>
2016-06-17 16:55:24 +0200

    Début modification formulaire de demande de matériel/logiciel.

--- Symfony/src/NoxIntranet/SupportSIBundle/Controller/SupportSIController.php
+++ Symfony/src/NoxIntranet/SupportSIBundle/Controller/SupportSIController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use NoxIntranet\SupportSIBundle\Entity\CompteurDemande;
 use NoxIntranet\SupportSIBundle\Entity\DemandeMateriel;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class SupportSIController extends Controller {
 
@@ -29,55 +31,68 @@ class SupportSIController extends Controller {
         return $this->redirect('http://' . $adresseCourante . '/Symfony/web/WikiNox/index.php');
     }
 
-    public function demandeAction() {
+    public function demandeAction(Request $request) {
+
+        // Récupère le profil du collaborateur actuel.
         $usr = $this->get('security.context')->getToken()->getUser();
 
+        // Récupère le nom d'utilisateur et l'agence du collaborateur actuel.
         $name = $usr->getUsername();
         $agence = $usr->getAgence();
+        $completName = $usr->getFirstname() . ' ' . $usr->getLastname();
 
-        static $numOrdre = 0;
-        $numOrdre++;
+        // Génére le formulaire de demande de matériel
+        $formBuilder = $this->get('form.factory')->createBuilder('form');
+        $formBuilder
+                ->add('agence', TextType::class, array(
+                    'data' => $agence
+                ))
+                ->add('demandeur', TextType::class, array(
+                    'data' => $completName
+                ))
+                ->add('materielCheckbox', CheckboxType::class, array(
+                    'label' => 'Matériel',
+                ))
+                ->add('logicielCheckbox', CheckboxType::class, array(
+                    'label' => 'Logiciel',
+                ))
+                ->add('materielList', ChoiceType::class, array(
+                    'choices' => array(
+                        'Portable' => 'Portable',
+                        'Ultra portable' => 'Ultra portable',
+                        'Poste fixe' => 'Poste fixe',
+                        'Poste CAO' => 'Poste CAO'
+                    ),
+                    'placeholder' => 'Séléctionnez un matériel',
+                    'required' => false
+                ))
+                ->add('logicielName', TextType::class, array(
+                    'required' => false
+                ))
+                ->add('raison', TextType::class)
+                ->add('date', DateType::class, array(
+                    'widget' => 'text'
+                ))
+                ->add('mailSuperieur', EmailType::class)
+                ->add('Valider', SubmitType::class)
+                ->add('Refuser', SubmitType::class)
+        ;
+        $form = $formBuilder->getForm();
+        $form->handleRequest($request);
 
-        date_default_timezone_set('Europe/Paris');
-        $date = date('d/m/Y à h:i:s', time());
-
-        return $this->render('NoxIntranetSupportSIBundle:Support:demandeMateriel.html.twig', array('username' => $name, 'agence' => $agence, 'date' => $date));
-    }
-
-    public function demandeCheckAction(Request $request) {
-        $usr = $this->get('security.context')->getToken()->getUser();
-
-        $em = $this->getDoctrine()->getManager();
-
-        $name = $usr->getUsername();
-        $agence = $usr->getAgence();
-
-        date_default_timezone_set('Europe/Paris');
-        $date = new \DateTime(date('Y/m/d'));
-
-        $emailDemandeur = preg_replace('/-.*/', '', $request->query->get('demandeur')) . "@groupe-nox.com";
-
-        if ($request->query->get('materiel') == null && $request->query->get('logicielCheckbox') == null) {
-            $request->getSession()->getFlashBag()->add('noticeErreur', "Veuillez choisir au moins un type de demande !");
-        } elseif ($request->query->get('logicielCheckbox') == 'logicielCheckbox' && ($request->query->get('logiciel') == null)) {
-            $request->getSession()->getFlashBag()->add('noticeErreur', "Veuillez renseigner le/les logiciel(s) désiré(s) !");
-        } elseif ($request->query->get('raisonDemande') == null) {
-            $request->getSession()->getFlashBag()->add('noticeErreur', "Veuillez renseigner la raison de votre demande !");
-        } elseif ($request->query->get('dateLivraison') == null) {
-            $request->getSession()->getFlashBag()->add('noticeErreur', "Veuillez renseigner la date de livraison désirée !");
-        } elseif ($request->query->get('emailSuperieur') == null) {
-            $request->getSession()->getFlashBag()->add('noticeErreur', "Veuillez renseigner l'email de votre supérieur hiérarchique !");
-        } elseif ($emailDemandeur === $request->query->get('emailSuperieur')) {
-            $request->getSession()->getFlashBag()->add('noticeErreur', "Veuillez un email valide pour votre supérieur hiérarchique !");
-        } else {
-
-            $dateDemande = $request->query->get('dateLivraison');
-
+        if ($form->isValid()) {
             $em = $this->getDoctrine()->getManager();
 
-            $compteur = $em->getRepository('NoxIntranetSupportSIBundle:CompteurDemande')->find(6);
+            // Génére la date du jour.
+            date_default_timezone_set('Europe/Paris');
+            $date = date('d/m/Y à h:i:s', time());
 
-            if ($compteur == null) {
+            // Génére l'email du collaborateur en fonction de son nom d'utilisateur.
+            $emailDemandeur = preg_replace('/-.*/', '', $name) . "@groupe-nox.com";
+
+            // Génére un compteur de demande ou met la date et la valeur à jour si il existe déjà.
+            $compteur = $em->getRepository('NoxIntranetSupportSIBundle:CompteurDemande')->find(6);
+            if ($compteur === null) {
                 $compteur = new CompteurDemande();
                 $compteur->setId(6);
                 $compteur->setCompteur(1);
@@ -86,7 +101,7 @@ class SupportSIController extends Controller {
                 $em->persist($compteur);
                 $em->flush();
             } else {
-                if ($compteur->getDate()->format('d/m/Y') != $date->format('d/m/Y')) {
+                if ($compteur->getDate()->format('d/m/Y') !== $date->format('d/m/Y')) {
                     $compteur->setCompteur(1);
                     $compteur->setDate($date);
                     $em->persist($compteur);
@@ -94,126 +109,132 @@ class SupportSIController extends Controller {
                 }
             }
 
+            // Génére un numéro pour la demande.
             $numRequette = $date->format('d/m/Y') . "-" . $compteur->getCompteur();
-            $superieur = $request->query->get('emailSuperieur');
 
-            $demandeur = $em->getRepository('NoxIntranetUserBundle:User')->findOneByUsername($request->query->get('demandeur'));
-            $nomDemandeur = $demandeur->getFirstname() . " " . $demandeur->getLastname();
-
+            // Génére une clé aléatoire pour identifier la demande.
             $cle = md5(uniqid(rand(), true));
             while (!(empty($em->getRepository('NoxIntranetSupportSIBundle:DemandeMateriel')->findOneByCleDemande($cle)))) {
                 $cle = md5(uniqid(rand(), true));
             }
 
+            // Récupère l'adresse du helpdesk.
+            $adresseHelpdesk = $em->getRepository('NoxIntranetAdministrationBundle:Helpdesk')->findOneBySection('Helpdesk')->getEmail();
+
+            // Génére l'email de demande pour la DSI et l'envoie.
             $message = \Swift_Message::newInstance()
-                    ->setSubject('Demande de matériel/logiciel ' . $nomDemandeur)
+                    ->setSubject('Demande de matériel/logiciel ' . $completName)
                     ->setFrom('noreply@groupe-nox.com')
-                    ->setTo($superieur)
+                    ->setTo($adresseHelpdesk)
                     ->setBody(
                     $this->renderView(
-                            'Emails/confirmationDemandeMateriel.html.twig', array('materiel' => $request->query->get('materiel'), 'logicielCheckbox' => $request->query->get('logicielCheckbox'), 'demandeur' => $nomDemandeur, 'agence' => $request->query->get('agence'),
-                        'numOrdre' => $numRequette, 'categorie' => $request->query->get('categorie'), 'logiciel' => $request->query->get('logiciel'), 'date' => $dateDemande, 'lienConfirmation' => $cle, 'raison' => $request->query->get('raisonDemande')
+                            'Emails/demandeMaterielDSI.html.twig', array('materiel' => $form->get('materielList')->getData(), 'logicielCheckbox' => $form->get('logicielCheckbox')->getData(), 'demandeur' => $name, 'agence' => $agence,
+                        'numOrdre' => $numRequette, 'logiciel' => $form->get('logicielName')->getData(), 'date' => $date, 'cle' => $cle, 'raison' => $form->get('raison')->getData()
                             )
                     ), 'text/html'
             );
             $this->get('mailer')->send($message);
 
+            // Incrémente le compteur de demande du jour.
             $compteur->setCompteur($compteur->getCompteur() + 1);
             $em->persist($compteur);
 
-            $donneesMessage = array('materiel' => $request->query->get('materiel'), 'logicielCheckbox' => $request->query->get('logicielCheckbox'), 'demandeur' => $nomDemandeur, 'agence' => $request->query->get('agence'),
-                'numOrdre' => $numRequette, 'categorie' => $request->query->get('categorie'), 'logiciel' => $request->query->get('logiciel'), 'emailSuperieur' => $superieur, 'raison' => $request->query->get('raisonDemande'),
-                'date' => $dateDemande, 'lienConfirmation' => $cle, 'emailDemandeur' => $emailDemandeur
+            // Stock les données de la demande.
+            $donneesMessage = array('materiel' => $form->get('materielList')->getData(), 'logicielCheckbox' => $form->get('logicielCheckbox')->getData(), 'demandeur' => $name, 'agence' => $agence,
+                'numOrdre' => $numRequette, 'logiciel' => $form->get('logicielName')->getData(), 'emailSuperieur' => $form->get('mailSuperieur')->getData(), 'raison' => $form->get('raison')->getData(),
+                'date' => $date, 'cle' => $cle, 'emailDemandeur' => $emailDemandeur
             );
 
+            // Stock la demande en base de donnée en attente de traitement.
             $newDemandeMateriel = new DemandeMateriel();
-
-            $newDemandeMateriel->setMailSuperieur($superieur);
+            $newDemandeMateriel->setMailSuperieur($form->get('mailSuperieur')->getData());
             $newDemandeMateriel->setMessage($donneesMessage);
             $newDemandeMateriel->setCleDemande($cle);
+            $newDemandeMateriel->setStatus('DSI');
             $em->persist($newDemandeMateriel);
-
             $em->flush();
 
-            $request->getSession()->getFlashBag()->add('notice', "Votre demande a été transmise a votre supérieur et est en attente de validation.");
-            return $this->render('NoxIntranetSupportSIBundle:Support:demandeMateriel.html.twig', array('username' => $name, 'agence' => $agence, 'date' => $date));
+            $request->getSession()->getFlashBag()->add('notice', "Votre demande a été transmise à la DSI et est en attente de validation.");
+            return $this->redirectToRoute('nox_intranet_demande_materiel');
         }
 
-        return $this->render('NoxIntranetSupportSIBundle:Support:demandeMateriel.html.twig', array('username' => $name, 'agence' => $agence, 'date' => $date));
+        return $this->render('NoxIntranetSupportSIBundle:Support:demandeMateriel.html.twig', array('username' => $name, 'agence' => $agence, 'date' => $date, 'formDemandeMateriel' => $form));
     }
 
-    public function demandeConfirmationAction(Request $request, $cleDemande, $reponse) {
+    public function demandeConfirmationDSIAction(Request $request, $cleDemande) {
 
+        // Récupère la demande de matériel/logiciel
         $em = $this->getDoctrine()->getManager();
+        $demande = $em->getRepository('NoxIntranetSupportSIBundle:DemandeMateriel')->findOneBy(array('cleDemande' => $cleDemande, 'status' => 'DSI'));
 
-        $adresseHelpdesk = $em->getRepository('NoxIntranetAdministrationBundle:Helpdesk')->findOneBySection('Helpdesk')->getEmail();
-
-        $demande = $em->getRepository('NoxIntranetSupportSIBundle:DemandeMateriel')->findOneByCleDemande($cleDemande);
-
-        $donneesMessage = $demande->getMessage();
-
-        $formBuilder = $this->get('form.factory')->createBuilder('form', $texteEncart);
-
+        // Génére le formulaire de validation/refus de la demande avec posibilité de renseigner une estimation du prix.
+        $formBuilder = $this->get('form.factory')->createBuilder('form');
         $formBuilder
                 ->add('prix', TextType::class)
                 ->add('Valider', SubmitType::class)
                 ->add('Refuser', SubmitType::class)
         ;
-
         $form = $formBuilder->getForm();
 
-
+        // Si la demande est présente en base de données...
         if (!empty($demande)) {
             $form->handleRequest($request);
 
+            $donneesMessage = $demande->getMessage();
+            $mailSuperieur = $demande->getMessage();
+
             if ($form->isValid()) {
+                // Si la demande est validée par la DSI...
                 if ($form->get('Valider')->isClicked()) {
 
+                    // On envoi un email au supérieur hiérachique pour demander la confirmation d'achat.
                     $messageHelpdesk = \Swift_Message::newInstance()
                             ->setSubject('Demande de matériel/logiciel ' . $donneesMessage['demandeur'])
                             ->setFrom('noreply@groupe-nox.com')
-                            ->setTo($adresseHelpdesk)
+                            ->setTo($mailSuperieur)
                             ->setBody(
                             $this->renderView(
-                                    'Emails/demandeMateriel.html.twig', array('donneesMessage' => $donneesMessage)
+                                    'Emails/demandeMateriel.html.twig', array('donneesMessage' => $donneesMessage, 'prixEstime' => $form->get('prix')->getData())
                             ), 'text/html'
                     );
                     $this->get('mailer')->send($messageHelpdesk);
 
-                    $messageDemandeur = $messageDemandeur = \Swift_Message::newInstance()
-                            ->setSubject('Validation de votre demande de matériel')
-                            ->setFrom('noreply@groupe-nox.com')
-                            ->setTo($donneesMessage['emailDemandeur'])
-                            ->setBody("Bonjour " . $donneesMessage['demandeur'] . ", votre demande de matériel a été validée par votre supérieur et transmise au service Helpdesk."
-                            , 'text/html'
-                    );
-                    $this->get('mailer')->send($messageDemandeur);
-
-                    $em->remove($demande);
+                    // On modifie le status de la demande pour que la DSI n'y ai plus accés.
+                    $demande->setStatus('SupérieurHiérarchique');
+                    $em->persist($demande);
                     $em->flush();
 
                     $request->getSession()->getFlashBag()->add('notice', "La demande de matériel de " . $donneesMessage['demandeur'] . " a été validée.");
+                    return $this->redirectToRoute('nox_intranet_accueil');
+
+                    // ...Si la demande est refusée par la DSI.
                 } else {
+                    // On envoi un email au collaborateur pour lui indiquer le refus.
                     $messageDemandeur = $messageDemandeur = \Swift_Message::newInstance()
                             ->setSubject('Rejet de votre demande de matériel')
                             ->setFrom('noreply@groupe-nox.com')
                             ->setTo($donneesMessage['emailDemandeur'])
-                            ->setBody("Bonjour " . $donneesMessage['demandeur'] . ", votre demande de matériel a été rejeté par votre supérieur."
+                            ->setBody("Bonjour " . $donneesMessage['demandeur'] . ", votre demande de matériel a été rejeté par la DSI."
                             , 'text/html'
                     );
                     $this->get('mailer')->send($messageDemandeur);
 
+                    // On supprime la demande.
                     $em->remove($demande);
                     $em->flush();
 
                     $request->getSession()->getFlashBag()->add('notice', "La demande de matériel de " . $donneesMessage['demandeur'] . " a été rejeté.");
+                    return $this->redirectToRoute('nox_intranet_accueil');
                 }
             }
+
+            // ...Si la demande n'existe plus en base de données.
         } else {
             $request->getSession()->getFlashBag()->add('notice', "Cette demande de matériel a déjà été traitée.");
+            return $this->redirectToRoute('nox_intranet_accueil');
         }
 
-        return $this->redirectToRoute('nox_intranet_accueil');
+        return $this->render();
     }
 
 }
