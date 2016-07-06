d8722b8551944c8d889986d2b99b94a6cf82ca1a
tristanbsn <tristan.besson@wanadoo.fr>
2016-07-06 17:51:33 +0200

    Ajout d'un éditeur de texte dans la section NOX Virtual Reality.

--- Symfony/src/NoxIntranet/RessourcesBundle/Controller/RessourcesController.php
+++ Symfony/src/NoxIntranet/RessourcesBundle/Controller/RessourcesController.php
@@ -719,4 +719,45 @@ class RessourcesController extends Controller {
         return $this->render('NoxIntranetRessourcesBundle:RH:affichageExcel.html.twig', array('news' => $fichierExcel, 'dossier' => $dossier, 'chemin' => $chemin, 'config' => $config));
     }
 
+    public function NOXVirtualRealityAction(Request $request) {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $texteEncart = $em->getRepository('NoxIntranetAdministrationBundle:texteEncart')->findOneBySection('NOXVR');
+
+        if ($texteEncart == null) {
+            $texteEncart = new texteEncart();
+            $texteEncart->setSection('NOXVR');
+            $em->persist($texteEncart);
+            $em->flush();
+        }
+
+        $formBuilder = $this->get('form.factory')->createBuilder('form', $texteEncart);
+
+        $formBuilder
+                ->add('text', 'ckeditor')
+                ->add('modifier', 'submit')
+        ;
+
+        $form = $formBuilder->getForm();
+
+        $form->handleRequest($request);
+
+        // On vérifie que les valeurs entrées sont correctes
+        // (Nous verrons la validation des objets en détail dans le prochain chapitre)
+        if ($form->isValid()) {
+            // On l'enregistre notre objet $advert dans la base de données, par exemple
+
+            $em->persist($texteEncart);
+            $em->flush();
+
+            // On redirige vers la page de visualisation de l'annonce nouvellement créée
+            return $this->redirectToRoute('nox_intranet_nox_virtual_reality');
+        }
+
+        $texte = $texteEncart->getText();
+
+        return $this->render('NoxIntranetRessourcesBundle:VR:NoxVirtualReality.html.twig', array('texte' => $texte, 'formulaire' => $form->createView()));
+    }
+
 }
