78b6daadcb222ed359264d138a1321ac3ba80bd9
tristanbsn <tristan.besson@wanadoo.fr>
2016-09-15 11:00:28 +0200

    Changemenet mot de passe administrateur pour les scripts.

--- Symfony/scripts/Planification_Taches.bat
+++ Symfony/scripts/Planification_Taches.bat
@@ -1,15 +1,15 @@
-SCHTASKS /Create /RU Administrateur /RP Nox30intr& /SC HOURLY /ST 08:00:00 /TN PDFParsing /TR %~d0\wamp\www\Symfony\scripts\pdfParsing.bat /RL HIGHEST
+SCHTASKS /Create /RU "Administrateur" /RP "Nox30intr&" /SC HOURLY /ST 08:00:00 /TN PDFParsing /TR %~d0\wamp\www\Symfony\scripts\pdfParsing.bat /RL HIGHEST
 
-SCHTASKS /Create /RU Administrateur /RP Nox30intr& /SC HOURLY /ST 08:00:00 /TN ListingKeywordsReferences /TR %~d0\wamp\www\Symfony\scripts\listingKeywordsReferences.bat /RL HIGHEST
+SCHTASKS /Create /RU "Administrateur" /RP "Nox30intr&" /SC HOURLY /ST 08:00:00 /TN ListingKeywordsReferences /TR %~d0\wamp\www\Symfony\scripts\listingKeywordsReferences.bat /RL HIGHEST
 
-SCHTASKS /Create /RU Administrateur /RP Nox30intr& /SC HOURLY /ST 08:00:00 /TN GXDataUpdating /TR %~d0\wamp\www\Symfony\scripts\GXDataUpdating.bat /RL HIGHEST
+SCHTASKS /Create /RU "Administrateur" /RP "Nox30intr&" /SC HOURLY /ST 08:00:00 /TN GXDataUpdating /TR %~d0\wamp\www\Symfony\scripts\GXDataUpdating.bat /RL HIGHEST
 
-SCHTASKS /Create /RU Administrateur /RP Nox30intr& /SC HOURLY /ST 08:00:00 /TN GXAffairesUpdating /TR %~d0\wamp\www\Symfony\scripts\GXAffairesUpdating.bat /RL HIGHEST
+SCHTASKS /Create /RU "Administrateur" /RP "Nox30intr&" /SC HOURLY /ST 08:00:00 /TN GXAffairesUpdating /TR %~d0\wamp\www\Symfony\scripts\GXAffairesUpdating.bat /RL HIGHEST
 
-SCHTASKS /Create /RU Administrateur /RP Nox30intr& /SC HOURLY /ST 08:00:00 /TN RHHierarchieExtraction /TR %~d0\wamp\www\Symfony\scripts\RHHierarchieExtraction.bat /RL HIGHEST
+SCHTASKS /Create /RU "Administrateur" /RP "Nox30intr&" /SC HOURLY /ST 08:00:00 /TN RHHierarchieExtraction /TR %~d0\wamp\www\Symfony\scripts\RHHierarchieExtraction.bat /RL HIGHEST
 
-SCHTASKS /Create /RU Administrateur /RP Nox30intr& /SC DAILY /ST 08:00:00 /TN RecupUsers /TR %~d0\wamp\www\Symfony\scripts\recupUsers.bat /RL HIGHEST
+SCHTASKS /Create /RU "Administrateur" /RP "Nox30intr&" /SC DAILY /ST 08:00:00 /TN RecupUsers /TR %~d0\wamp\www\Symfony\scripts\recupUsers.bat /RL HIGHEST
 
-SCHTASKS /Create /RU Administrateur /RP Nox30intr& /SC DAILY /ST 08:00:00 /TN BDDDump /TR %~d0\wamp\www\Symfony\scripts\BDDDump.bat /RL HIGHEST
+SCHTASKS /Create /RU "Administrateur" /RP "Nox30intr&" /SC DAILY /ST 08:00:00 /TN BDDDump /TR %~d0\wamp\www\Symfony\scripts\BDDDump.bat /RL HIGHEST
 
-SCHTASKS /Create /RU Administrateur /RP Nox30intr& /SC DAILY /ST 08:00:00 /TN AccueilRandomImage /TR %~d0\wamp\www\Symfony\scripts\AccueilRandomImage.bat /RL HIGHEST
+SCHTASKS /Create /RU "Administrateur" /RP "Nox30intr&" /SC DAILY /ST 08:00:00 /TN AccueilRandomImage /TR %~d0\wamp\www\Symfony\scripts\AccueilRandomImage.bat /RL HIGHEST
