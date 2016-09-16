7cd436d198582db44533774ca5cdf150ab5b1f13
tristanbsn <tristan.besson@wanadoo.fr>
2016-09-16 17:32:41 +0200

    Ajout d'animation et de blocage lors de l'envoi de message + scrolling sur le dernier message après l'envoi.

--- Symfony/vendor/composer/autoload_classmap.php
+++ Symfony/vendor/composer/autoload_classmap.php
@@ -1823,6 +1823,8 @@ return array(
     'NoxIntranet\\RessourcesBundle\\Entity\\ProfilsRepository' => $baseDir . '/src/NoxIntranet/RessourcesBundle/Entity/ProfilsRepository.php',
     'NoxIntranet\\RessourcesBundle\\Entity\\PropositionPrestation' => $baseDir . '/src/NoxIntranet/RessourcesBundle/Entity/PropositionPrestation.php',
     'NoxIntranet\\RessourcesBundle\\Entity\\PropositionPrestationRepository' => $baseDir . '/src/NoxIntranet/RessourcesBundle/Entity/PropositionPrestationRepository.php',
+    'NoxIntranet\\RessourcesBundle\\Entity\\Proposition_Echanges' => $baseDir . '/src/NoxIntranet/RessourcesBundle/Entity/Proposition_Echanges.php',
+    'NoxIntranet\\RessourcesBundle\\Entity\\Proposition_EchangesRepository' => $baseDir . '/src/NoxIntranet/RessourcesBundle/Entity/Proposition_EchangesRepository.php',
     'NoxIntranet\\RessourcesBundle\\Entity\\RecherchePrestation' => $baseDir . '/src/NoxIntranet/RessourcesBundle/Entity/RecherchePrestation.php',
     'NoxIntranet\\RessourcesBundle\\Entity\\RecherchePrestationRepository' => $baseDir . '/src/NoxIntranet/RessourcesBundle/Entity/RecherchePrestationRepository.php',
     'NoxIntranet\\RessourcesBundle\\Entity\\ReferencesKeywords' => $baseDir . '/src/NoxIntranet/RessourcesBundle/Entity/ReferencesKeywords.php',
