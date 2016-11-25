1f6ad25e6f1201580d5293761255864f525484f2
tristanbsn <tristan.besson@wanadoo.fr>
2016-11-25 14:04:02 +0100

    Correctif bug lié au '-' sur la récupération des utilisateur dans l'active directory.

--- Symfony/scripts/recupUsers.bat
+++ Symfony/scripts/recupUsers.bat
@@ -3,5 +3,3 @@ cd %~d0\wamp\www\Symfony
 csvde -f %~d0/wamp/www/Symfony/users.csv -d "dc=nox,dc=local" -r "(&(objectClass=User)(objectCategory=person)(!objectCategory=computer))" -l "sAMAccountName"
 
 php app/console noxintranet:majdb
-
-pause
