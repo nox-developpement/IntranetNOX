5bddf9912758a1c8e72405b7c54e264c96e76c20
tristanbsn <tristan.besson@wanadoo.fr>
2017-01-27 11:39:50 +0100

    Amélioration majeur sur le sytème d'extraction des collaborateurs de l'AD.

--- Symfony/scripts/recupUsers.bat
+++ Symfony/scripts/recupUsers.bat
@@ -1,5 +1,7 @@
 cd %~d0\wamp\www\Symfony
 
-csvde -f %~d0/wamp/www/Symfony/users.csv -d "dc=nox,dc=local" -r "(&(objectClass=User)(objectCategory=person)(!objectCategory=computer))" -l "sAMAccountName"
+csvde -f %~d0/wamp/www/Symfony/users.csv -d "dc=nox,dc=local" -r "(&(objectClass=User)(objectCategory=person)(!objectCategory=computer))" -l "sAMAccountName, sn, givenName"
 
 php app/console noxintranet:majdb
+
+pause
