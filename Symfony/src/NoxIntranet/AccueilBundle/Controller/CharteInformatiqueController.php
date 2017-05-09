2f1d952859986290b150fbba22500391d4633488
tristanbsn <tristan.besson@wanadoo.fr>
2017-05-09 15:04:18 +0200

    Ajout d'une redirection vers la charte informatique si celle-ci n'est pas déjà lu.

--- Symfony/src/NoxIntranet/AccueilBundle/Controller/CharteInformatiqueController.php
+++ Symfony/src/NoxIntranet/AccueilBundle/Controller/CharteInformatiqueController.php
@@ -9,29 +9,50 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CharteInformatiqueController extends Controller {
 
+    /**
+     * 
+     * Affiche la charte informatique et permet de valider sa lecture.
+     * 
+     * @param Request $request
+     * @return View
+     */
     public function displayCharteInformatiqueAction(Request $request) {
-
+        // Génération du formulaire de charte informatique.
         $formCharteInformatiqueBuilder = $this->get('form.factory')->createBuilder();
         $formCharteInformatiqueBuilder
-                ->add('Read', CheckboxType::class)
+                ->add('Read', CheckboxType::class, array(
+                    'label' => "J'ai lu et j'approuve la charte informatique.",
+                    'attr' => array(
+                        'title' => "Faite défiler la charte en entier pour pouvoir cocher la case."
+                    ),
+                    'label_attr' => array(
+                        'title' => "Faite défiler la charte en entier pour pouvoir cocher la case."
+                    )
+                ))
                 ->add('Validate', SubmitType::class, array(
                     'label' => 'Valider'
         ));
         $formCharteInformatique = $formCharteInformatiqueBuilder->getForm();
 
+        // Traitement du formulaire.
         $formCharteInformatique->handleRequest($request);
         if ($formCharteInformatique->isValid()) {
-            $em = $this->getDoctrine()->getManager();
+            // On récupére l'utilisateur courant.
             $user = $this->get('security.token_storage')->getToken()->getUser();
 
+            // On indique qu'il a lu la charte informatique.
             $user->setHasReadCharteInformatique(true);
 
+            // On sauvegarde les changement en base de données.
+            $em = $this->getDoctrine()->getManager();
             $em->flush();
 
-            //var_dump($request->getReferer());
-            return $this->redirect($request->getReferer());
+            // On récupère la dernière page visité et on redirige vers celle-ci.
+            $lastRoute = $request->get('lastRoute');
+            return $this->redirectToRoute($lastRoute);
         }
 
+        // Affichage de la charte informatique et du formulaire de validation de lecture.
         return $this->render('NoxIntranetAccueilBundle:CharteInformatique:charteInformatique.html.twig', array('formCharteInformatique' => $formCharteInformatique->createView()));
     }
 
--- Symfony/src/NoxIntranet/AccueilBundle/MessageAlert/CharteInformatiqueListener.php
+++ Symfony/src/NoxIntranet/AccueilBundle/MessageAlert/CharteInformatiqueListener.php
@@ -5,31 +5,58 @@ namespace NoxIntranet\AccueilBundle\MessageAlert;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class CharteInformatiqueListener {
 
-    public function __construct(TokenStorageInterface $token, AuthorizationCheckerInterface $authorization) {
+    /**
+     * 
+     * Constructeur pour les services utilisés dans la class
+     * 
+     * @param TokenStorageInterface $token
+     * @param AuthorizationCheckerInterface $authorization
+     * @param ContainerInterface $container
+     */
+    public function __construct(TokenStorageInterface $token, AuthorizationCheckerInterface $authorization, ContainerInterface $container) {
         $this->token = $token;
         $this->authorization = $authorization;
+        $this->container = $container;
     }
 
+    /**
+     * 
+     * Vérifie que l'utilisateur à signer la charte informatique et redirige vers elle si ce n'est pas le cas.
+     * 
+     * @param FilterControllerEvent $event L'action effectué par l'utilisateur.
+     * @return Controller
+     */
     public function processCharteInformatique(FilterControllerEvent $event) {
         $token = $this->token;
         $authorization = $this->authorization;
+        $container = $this->container;
 
-        if (!($event->isMasterRequest() && $token->getToken() && ($authorization->isGranted('IS_AUTHENTICATED_REMEMBERED') || $authorization->isGranted('IS_AUTHENTICATED_FULLY')))) {
-            return;
+        // Si l'event n'est pas une requette principal ou si l'utilisateur n'est pas identifié ou si l'utilisateur est déjà sur la charte informatique...
+        if (!($event->isMasterRequest() && $token->getToken() && ($authorization->isGranted('IS_AUTHENTICATED_REMEMBERED') || $authorization->isGranted('IS_AUTHENTICATED_FULLY'))) ||
+                $event->getRequest()->get('_route') === "nox_intranet_charte_informatique") {
+            return; // On quitte la fonction.
         }
 
+        // On récupére l'utilisateur.
         $user = $token->getToken()->getUser();
 
+        // Si l'utilisateur a déjà signé la charte informatique...
         if ($user->getHasReadCharteInformatique()) {
-            return;
+            return; // On quitte la fonction.
         }
-        
-        $controller = new \NoxIntranet\AccueilBundle\Controller\CharteInformatiqueController();
 
-        $event->setController($controller->displayCharteInformatiqueAction($event->getRequest()));
+        // On génère l'url de la charte informatique avec la dernière page visité en paramètre.
+        $redirectUrl = $container->get('router')->generate('nox_intranet_charte_informatique', array('lastRoute' => $event->getRequest()->get('_route')));
+
+        // On redirige vers la charte informatique.
+        $event->setController(function() use ($redirectUrl) {
+            return new RedirectResponse($redirectUrl);
+        });
     }
 
 }
