888f7f2d1cd034fafea11935170ef6e3cd94ae9d
tristanbsn <tristan.besson@wanadoo.fr>
2017-06-21 17:21:33 +0200

    Début extraction et affichage des statistiques d'utilisation des comptes VPN.

---
+++ Symfony/src/NoxIntranet/AdministrationBundle/Controller/StatsVPN/StatsVPNController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace NoxIntranet\AdministrationBundle\Controller\StatsVPN;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use DateTime;
+
+class StatsVPNController extends Controller {
+
+    public function uploadVPNLogs(Request $request) {
+        
+    }
+
+    public function statsVPNCalculationAction() {
+        // Dossier des fichiers de statistiques des VPN.
+        $vpnStatsFilesFolder = "C:/Users/t.besson/Downloads/Stats VPN";
+
+        // Tableau contenant les données pour les statistiques.
+        $statsDataByUsers = array();
+        $statsDataByMonths = array();
+
+        $objPHPExcel = \PHPExcel_IOFactory::load($vpnStatsFilesFolder . "/201706-GestionEffectif-ExportVPN.xlsx");
+        $objWorksheet = $objPHPExcel->getActiveSheet();
+
+        $idToName = array();
+
+        foreach ($objWorksheet->getRowIterator() as $rowIndex => $row) {
+            $name = $objWorksheet->getCell("B" . $rowIndex)->getValue();
+            $id = $objWorksheet->getCell("B" . $rowIndex)->getValue();
+
+            $idToName[$id] = $name;
+
+            $statsDataByMonths["01"][$id] = null;
+            $statsDataByMonths["02"][$id] = null;
+            $statsDataByMonths["03"][$id] = null;
+            $statsDataByMonths["04"][$id] = null;
+            $statsDataByMonths["05"][$id] = null;
+            $statsDataByMonths["06"][$id] = null;
+        }
+
+        // Pour chaques fichiers du dossier de VPN...
+        foreach (scandir($vpnStatsFilesFolder) as $file) {
+            // On récupére le chemin et l'extension du fichier.
+            $filePath = $vpnStatsFilesFolder . "/" . $file;
+            $extension = pathinfo($filePath, PATHINFO_EXTENSION);
+
+            // Si c'est un fichier zip...
+            if ($extension === "zip") {
+                // On ouvre l'archive.
+                $zip = new \ZipArchive();
+                $zip->open($filePath);
+
+                // On récupére le fichier de journal de connection.
+                $statsFileHandler = $zip->getStream("Statistiques_acces.csv");
+
+                // Pour chaque lignes du fichier CSV...
+                while (($data = fgetcsv($statsFileHandler, 0, ";")) !== FALSE) {
+                    // Si l'utilisateur n'est pas le System et qu'il s'agit d'une connexion...
+                    if ($data[2] !== "System" && !empty($data[8])) {
+                        // Récupération de la date.
+                        $date = DateTime::createFromFormat("Y-m-d H:i:s", $data[0]);
+                        $annee = $date->format("Y");
+                        $mois = $date->format("m");
+
+                        // On ajoute l'utilisateur au tableau.
+                        $statsDataByUsers[$data[2]]["Utilisateur"] = $data[2];
+
+                        // On ajoute la date de connexion au tableau par utilisateurs.
+                        $statsDataByUsers[$data[2]]["Connexions"]["Dates"][] = $date;
+                        $statsDataByUsers[$data[2]]["Connexions"]["Annees"][$annee]["Dates"][] = $date;
+                        $statsDataByUsers[$data[2]]["Connexions"]["Annees"][$annee]["Mois"][$mois]["Dates"][] = $date;
+
+                        // On ajoute la date au tableau par mois.
+                        $statsDataByMonths[$mois][$data[2]][] = $date;
+                    }
+                }
+            }
+        }
+
+        asort($statsDataByUsers);
+        //asort($statsDataByMonths);
+
+        $numberToMonth = array("01" => "Janvier", "02" => "Février", "03" => "Mars", "04" => "Avril", "05" => "Mai", "06" => "Juin", "07" => "Juillet", "08" => "Août", "09" => "Septembre", "10" => "Octobre", "11" => "Novembre", "12" => "Décembre");
+
+        return $this->render("NoxIntranetAdministrationBundle:StatsVPN:statsVPN.html.twig", array('statsDataByUsers' => $statsDataByUsers, 'statsDataByMonths' => $statsDataByMonths, 'numberToMonth' => $numberToMonth, 'idToName' => $idToName));
+    }
+
+}
