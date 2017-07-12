94fea7523d564cff0b1748ac796539000cdf4687
nox-developpement <t.besson@groupe-nox.com>
2017-07-12 17:41:48 +0200

    Travail sur le css du header.

--- Symfony/web/js/menuAnimation.js
+++ Symfony/web/js/menuAnimation.js
@@ -1,20 +1,3 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-function menuAnimation() {
-
-    $('.liens').mouseover(function () {
-        $(this).animate({'color': 'rgb(31,78,121)', 'background-color': '#fff'}, '400');
-    });
-
-    $('.liens').mouseout(function () {
-        $(this).animate({'background-color': 'rgb(31,78,121)', 'color': '#fff'}, '400');
-    });
-}
-
 function DivNewsSliding() {
     if ($('.DivMaj').length) {
         $('.DivMaj').delay(10000).each(function (index) {
