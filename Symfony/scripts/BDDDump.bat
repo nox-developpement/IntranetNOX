bace0c4b1b5ab1f59707c04b6b2b5e0cb85f80e5
LASSIAZ Théo <t.lassiaz@nox.local>
2017-08-17 11:37:35 +0200

    script bdddump

--- Symfony/scripts/BDDDump.bat
+++ Symfony/scripts/BDDDump.bat
@@ -15,8 +15,6 @@ set day=%timestamp:~6,2%
 cd "%~d0\wamp\bin\mysql\mysql5.6.17\bin"
 
 
-
-
 :: Extraction des bases de données au format .sql.
 echo Extraction des bases de données...
 mysqldump --user=intranetadmin --password=intranet --databases intranet --default-character-set=utf8 --result-file="%~d0\wamp\www\Symfony\SauvegardeBDDVersioning\BDD[Intranet][%day%-%month%-%year%].sql"
@@ -30,7 +28,6 @@ echo Compression des bases de données...
 7z a -tzip "%~d0\wamp\www\Symfony\SauvegardeBDDVersioning\Support\%year%\%month%\BDD[Support][%day%-%month%-%year%].zip" "%~d0\wamp\www\Symfony\SauvegardeBDDVersioning\BDD[Support][%day%-%month%-%year%].sql" -mm=LZMA -mx=9 -sdel
 
 
-
 :: Récupération de la date du dernier jour du mois courant dans la variable %errorlevel%.
 call :DaysOfMonth %year% %month%
 set lastDayOfMonth=%errorlevel%
@@ -42,30 +39,6 @@ if NOT %day% == %lastDayOfMonth% (
 	for /f "skip=1 eol=: delims=" %%F in ('dir /b /o-d "%~d0\wamp\www\Symfony\SauvegardeBDDVersioning\Intranet\%year%\%month%\*.zip"') do @del "%~d0\wamp\www\Symfony\SauvegardeBDDVersioning\Intranet\%year%\%month%\%%F"
 	for /f "skip=1 eol=: delims=" %%F in ('dir /b /o-d "%~d0\wamp\www\Symfony\SauvegardeBDDVersioning\Wiki\%year%\%month%\*.zip"') do @del "%~d0\wamp\www\Symfony\SauvegardeBDDVersioning\Wiki\%year%\%month%\%%F"
 	for /f "skip=1 eol=: delims=" %%F in ('dir /b /o-d "%~d0\wamp\www\Symfony\SauvegardeBDDVersioning\Support\%year%\%month%\*.zip"') do @del "%~d0\wamp\www\Symfony\SauvegardeBDDVersioning\Support\%year%\%month%\%%F"
-)
-
-
-
-echo Extraction des bases de données save externe...
-mysqldump --user=intranetadmin --password=intranet --databases intranet --default-character-set=utf8 --result-file="S:\BDD[Intranet][%day%-%month%-%year%].sql"
-mysqldump --user=intranetadmin --password=intranet --databases wikinox --default-character-set=utf8 --result-file="S:\BDD[Wiki][%day%-%month%-%year%].sql"
-mysqldump --user=intranetadmin --password=intranet --databases support --default-character-set=utf8 --result-file="S:\BDD[Support][%day%-%month%-%year%].sql"
-
-
-:: Compression des base de données en .zip.
-echo Compression des bases de données...
-7z a -tzip "S:\Intranet\%year%\%month%\BDD[Intranet][%day%-%month%-%year%].zip" "S:\BDD[Intranet][%day%-%month%-%year%].sql" -mm=LZMA -mx=9 -sdel
-7z a -tzip "S:\Wiki\%year%\%month%\BDD[Wiki][%day%-%month%-%year%].zip" "S:\BDD[Wiki][%day%-%month%-%year%].sql" -mm=LZMA -mx=9 -sdel
-7z a -tzip "S:\Support\%year%\%month%\BDD[Support][%day%-%month%-%year%].zip" "S:\BDD[Support][%day%-%month%-%year%].sql" -mm=LZMA -mx=9 -sdel
-
-
-:: Si la date du jour correspond au dernier jour du mois...
-if NOT %day% == %lastDayOfMonth% (
-	:: On supprime toutes les sauvegardes du mois sauf la dernière.
-	echo Nettoyage des dossiers de sauvegarde...
-	for /f "skip=1 eol=: delims=" %%F in ('dir /b /o-d "S:\Intranet\%year%\%month%\*.zip"') do @del "S:\Intranet\%year%\%month%\%%F"
-	for /f "skip=1 eol=: delims=" %%F in ('dir /b /o-d "S:\Wiki\%year%\%month%\*.zip"') do @del "S:\Wiki\%year%\%month%\%%F"
-	for /f "skip=1 eol=: delims=" %%F in ('dir /b /o-d "S:\Support\%year%\%month%\*.zip"') do @del "S:\Support\%year%\%month%\%%F"
 )
 
 
